82cd9481fabb989b6e26af5fd0d10932772d6742
brgndy <brgndy96@gmail.com>
2023-04-03 00:30:47 +0900

    darkmode

--- dark-mode/src/components/Header/Header.tsx
+++ dark-mode/src/components/Header/Header.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import classes from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const darkModeHandler = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.setAttribute("data-theme", "light");
+      console.log(document.body.dataset);
+    } else {
+      document.body.setAttribute("data-theme", "dark");
+      console.log(document.body.dataset);
+    }
+  };
   return (
     <>
       <div className={classes.header_container}>
-        <h1>헤더입니다.</h1>
+        <h1>헤더섹션</h1>
+        <button onClick={darkModeHandler}>
+          {isDarkMode ? "light-mode" : "dark-mode"}
+        </button>
       </div>
     </>
   );
